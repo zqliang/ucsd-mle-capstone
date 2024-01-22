c7e1acf87c101c93b791398cf6572ab9d97e4375
zqliang <liangjessy@gmail.com>
2024-01-21 23:57:31 -0800

    include one-hot encoding

--- 8_prototype/recommender.py
+++ 8_prototype/recommender.py
@@ -3,7 +3,9 @@ import pandas as pd
 import numpy as np
 import os
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics.pairwise import cosine_similarity
+from scipy.sparse import hstack
 
 
 curr_path = os.path.dirname(os.path.abspath(__file__))
@@ -13,15 +15,69 @@ data_file_path = os.path.join(os.path.dirname(curr_path), 'data', 'anime-dataset
 df_anime = pd.read_csv(data_file_path)
 print(df_anime['Synopsis'].head())
 
+
+#basic filtering for duplicates
+duplicates_all = df_anime[df_anime.duplicated()]
+print("All Duplicates:")
+print(len(duplicates_all))
+
+duplicates = df_anime[df_anime.duplicated(['Name'])].sort_values(by='Name')
+print("Duplicates based on Name:")
+print(len(duplicates))
+duplicates = duplicates[['anime_id', 'Name']]
+print(duplicates)
+
+df_anime_new = df_anime.drop_duplicates(['Name'])
+print("Cleaned anime shape: {} \n".format(df_anime_new.shape))
+print("Old anime shape: {}".format(df_anime.shape))
+
+
+
+#filter out certain genre
+to_exclude = df_anime[df_anime['Genres'].str.contains('Hentai', case=False, na=False)]
+filtered_df = df_anime[~df_anime.index.isin(to_exclude.index)]
+
+
+# Convert Name column to lowercase and remove spaces
+filtered_df['Processed_Name'] = filtered_df['Name'].str.lower().replace(' ', '')
+
+# Filter out rows with titles in lowercase and without spaces
+duplicate_rows = filtered_df[filtered_df.duplicated(subset='Processed_Name', keep=False) | 
+                             ~filtered_df.duplicated(subset='Processed_Name', keep=False) & 
+                             ~filtered_df['Processed_Name'].str.contains(' ')]
+
+# Filter out rows that are upper case and have no spacing, e.g. between Death Note and DEATHNOTE, keep Death Note
+filtered_df = filtered_df[~((filtered_df['Processed_Name'].isin(duplicate_rows['Processed_Name'])) 
+                            & (filtered_df.duplicated(subset='Processed_Name', keep=False)))]
+
+# Drop the intermediate 'Processed_Name' column
+filtered_df = filtered_df.drop(columns='Processed_Name')
+
+#drop rows with unknown genres
+unknown_rows = filtered_df[filtered_df['Genres'].str.lower() == 'unknown']
+filtered_df = filtered_df.drop(unknown_rows.index)
+
+
 # Create the TF-IDF matrix for text comparison
 tfidf = TfidfVectorizer(stop_words='english')
-tfidf_matrix = tfidf.fit_transform(df_anime['Synopsis'])
+synopsis_vectors = tfidf.fit_transform(filtered_df['Synopsis'])
+
+#use one-hot encoder to include genre in the recommendation
+encoder = OneHotEncoder(sparse_output=True)
+
+genre_encoded_sparse = encoder.fit_transform(filtered_df[['Genres']].explode('Genres'))
+
+# Step 4: Combine the sparse matrices horizontally (hstack)
+combined_sparse_matrix = hstack([genre_encoded_sparse, synopsis_vectors])
+
+
+
 
 # Compute cosine similarity between all anime synopses
-similarity = cosine_similarity(tfidf_matrix)
+similarity = cosine_similarity(combined_sparse_matrix)
 similarity_df = pd.DataFrame(similarity, 
-                             index=df_anime['Name'], 
-                             columns=df_anime['Name'])
+                             index=filtered_df['Name'], 
+                             columns=filtered_df['Name'])
 
 anime_list = similarity_df.columns.values
 
@@ -30,7 +86,7 @@ anime_list = similarity_df.columns.values
 st.title("Anime Recommender")
 
 #sidebar with user input
-input_anime = st.sidebar.selectbox("Select an anime:", df_anime['Name'].values)
+input_anime = st.sidebar.selectbox("Select an anime:", filtered_df['Name'].values)
 
 # Number of recommendations to show
 top_n = st.sidebar.slider("Number of recommendations", 1, 30, 10)
@@ -44,7 +100,7 @@ def content_anime_recommender(input_anime, similarity_database=similarity_df, an
 
 #get image url
 def get_image_url(anime_name):
-    return df_anime.loc[df_anime['Name'] == anime_name, 'Image URL'].values[0]
+    return filtered_df.loc[filtered_df['Name'] == anime_name, 'Image URL'].values[0]
 
 #display recommendations
 recommended_anime = content_anime_recommender(input_anime, top_n=top_n)
